f6d03dc5f938a5403d7223667cf51c3492e3ae6d
qishichang <qishichang@live.com>
2019-12-27 23:56:21 +1300

    Add Header component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -7,7 +7,6 @@ export default class Todos extends Component {
   render() {
     return (
       <div>
-        <h1>Todos</h1>
         {this.props.todos.map(todo => (
           <Todo key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
         ))}
