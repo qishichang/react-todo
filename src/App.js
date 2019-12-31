065ee6ca7b52ad78eb330a775eac91d13bb89a5a
qishichang <qishichang@live.com>
2019-12-31 18:26:31 +1300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import uuid from 'uuid';
@@ -6,6 +7,7 @@ import uuid from 'uuid';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
 
 class App extends Component {
   state = {
@@ -64,15 +66,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Header />
-        <AddTodo addTodo={this.addTodo} />
-        <Todos
-          todos={this.state.todos}
-          markComplete={this.markComplete}
-          delTodo={this.delTodo}
-        />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.markComplete}
+                    delTodo={this.delTodo}
+                  />
+                </>
+              )}
+            />
+            <Route path="/about" component={About}></Route>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is the TodoList app v0.0.1. It is part of a React crash course</p>
+    </>
+  );
+}
+
+export default About;
